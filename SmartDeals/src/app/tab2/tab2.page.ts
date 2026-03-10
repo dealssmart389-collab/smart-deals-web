4d7450aaf45212ac6ba27fde020396791c519b5a
اسم-المستخدم-الذي-خترته <->
2026-03-10 08:33:08 +0000

    تحديث تلقائي للمتجر الذكي: 2026-03-10 08:33 🚀

--- SmartDeals/src/app/tab2/tab2.page.ts
+++ SmartDeals/src/app/tab2/tab2.page.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { InteractionService } from '../services/interaction.service';
 
 @Component({ selector: 'app-tab2', templateUrl: 'tab2.page.html' })
 export class Tab2Page {
-  constructor(public blood: InteractionService) {}
+  products = [
+    { name: 'Redmi Note 15 Pro+', price: '4500 DH', image: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=400', tag: 'New' },
+    { name: 'Apple Watch Ultra 3', price: '8200 DH', image: 'https://images.unsplash.com/photo-1544117518-2b476dd354d2?w=400', tag: 'Limited' }
+  ];
+  constructor() {}
 }
