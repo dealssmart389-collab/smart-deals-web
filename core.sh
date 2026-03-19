6d033f1ad47bd0f41714ca73860ab139a36f1f30
dealssmart389-collab <mounirbenalal@gmail.com>
2026-03-19 20:06:29 +0000

    System Evolution: Thu Mar 19 20:06:29 +00 2026

--- core.sh
+++ core.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./evolve.sh
+find . -name "*.tmp" -type f -delete
 git remote set-url origin https://ghp_MGcabXzq9CopdyfsvlcwT3biTYKcfO0DDqwH@github.com/dealssmart389-collab/smart-deals-web.git
 git add .
-git commit -m "Update: $(date)"
+git commit -m "System Evolution: $(date)"
 git push origin main
